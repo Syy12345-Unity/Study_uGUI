ceab78874902a78c5de058c9a1dca3c6ce4fda13
Syy <shirakawa.yy@gmail.com>
2018-08-06 22:31:49 +0900

    Add demo03

---
+++ Assets/Scripts/TweenTransform.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenTransform : MonoBehaviour {
+    [SerializeField]
+    private TweenVector3 _tweenScale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_tweenScale.isUse)
+        {
+            transform.localScale = _tweenScale.Current(Time.realtimeSinceStartup);
+        }
+	}
+
+    [Serializable]
+    public class TweenVector3 {
+        public bool isUse;
+
+        public Vector3 startValue;
+        public Vector3 endValue;
+        public float duration = 1;
+        public float speed = 1;
+        public Vector3 Current(float time)
+        {
+            if(Mathf.Abs(duration) < Mathf.Epsilon)
+            {
+                return startValue;
+            }
+            return Vector3.Lerp(startValue,endValue,(time / duration) * speed  % 1);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -8,4 +8,9 @@ study uGUI
 
 ## Demo
 ![demo](ReadmeResource/demo01.gif)
+***
 ![demo](ReadmeResource/demo02.gif)
+***
+![demo](ReadmeResource/demo03.gif)
+Gauge bar move by tween of scale.x. Gauge bar anchor is stretch mode and Pivot is (0,0.5). so , if scale.x is 0, width is 0. scale.x is 1, width is gauge body width.
+If you control the width of the gauge bar directly by the program, you need to know the maximum gauge width, but this method is simple as it just moves the scale from 0 to 1.
