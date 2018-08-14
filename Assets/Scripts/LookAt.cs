5ff4b10cd9f9f3929315b26dc270510a7b000b4c
Syy <shirakawa.yy@gmail.com>
2018-08-14 23:11:58 +0900

    Add LookAt

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour {
+    [SerializeField]
+    Transform m_target;
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_target == null)
+        {
+            return;
+        }
+
+        transform.LookAt(m_target);
+	}
+}
