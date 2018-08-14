428b8fb64fda4a4f68ffe0de3924e882f353f599
Syy <shirakawa.yy@gmail.com>
2018-08-14 22:45:27 +0900

    Add TweenRotateY

---
+++ Assets/Scripts/TweenRotateY.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenRotateY : MonoBehaviour {
+    [SerializeField]
+    float duration = 10;
+	
+	// Update is called once per frame
+	void Update () {
+		var angle = transform.localRotation.eulerAngles;
+        angle.y += 360/duration * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(angle);
+	}
+}
